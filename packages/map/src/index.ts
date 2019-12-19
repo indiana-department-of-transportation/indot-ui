44de41d357f484ff84b30510810fc815d85c181a
jsmith <jasmith79@gmail.com>
2019-12-19 07:14:49 -0500

    Exposes TMCLeafletBase and defaultMarkerIcon, fixes the window.L typing

--- packages/map/src/index.ts
+++ packages/map/src/index.ts
@@ -1,3 +1,13 @@
+/**
+ * index.ts
+ *
+ * @description The main Leaflet for TMC React applications.
+ *
+ * @author jarsmith@indot.in.gov
+ * @license MIT
+ * @copyright INDOT, 2019
+ */
+
 // NOTE: this is ridiculous but arguably the best solution
 // since leaflet doesn't bundle this upstream.
 import 'leaflet/dist/leaflet.css';
@@ -5,9 +15,22 @@ import 'leaflet';
 import TMCMap from './TMCMap';
 import TMCPoly from './TMCPoly';
 import TMCMarker from './TMCMarker';
+import TMCLeafletBase from './TMCLeafletBase';
+import defaultMarkerIcon from './marker-icon';
 
+// Again, leaflet grrh argh.
+interface Window {
+  L: any,
+}
 const Leaflet = window.L;
 
-export { TMCMap, TMCPoly , TMCMarker, Leaflet };
+export {
+  TMCMap,
+  TMCPoly,
+  TMCMarker,
+  TMCLeafletBase,
+  defaultMarkerIcon,
+  Leaflet
+};
 
 export default TMCMap;
