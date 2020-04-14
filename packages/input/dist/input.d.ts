f0e555f3e6c6d70ea2e61c1b06598ad8835dec84
jasmith79 <jasmith79@gmail.com>
2020-04-14 17:20:33 -0400

    Rebuild artifacts

--- packages/input/dist/input.d.ts
+++ packages/input/dist/input.d.ts
@@ -1,25 +1,20 @@
-import React from 'react';
+/// <reference types="react" />
 import { FormControlEvent } from '@jasmith79/ts-utils';
-export interface IValidatingInputParams {
+import { RenderProps } from '@jasmith79/react-utils';
+export declare type ValidatingInputProps<T> = {
+    value: T;
+    setValue: (value: T | null) => void;
+    parse?: (input: string | null) => T | null;
+    format?: (value: T | null) => string;
+    onError?: (err: Error) => void;
+    name?: string;
+};
+export declare type ValidatingInputParams = {
     value: string;
     isError?: boolean;
     onBlur: (evt: FormControlEvent) => void;
     onChange: (evt: FormControlEvent) => void;
     name: string;
-}
-export interface IInputRenderFn {
-    (props: IValidatingInputParams): React.ReactElement;
-}
-export interface IValidatingInputProps<T> {
-    value: T;
-    setValue: (value: T | null) => void;
-    children?: React.ReactChild | IInputRenderFn;
-    render?: (props: IValidatingInputParams) => React.ReactElement;
-    component?: React.ComponentType<IValidatingInputParams>;
-    name?: string;
-    parse?: (input: string | null) => T | null;
-    format?: (value: T | null) => string;
-    onError?: (err: Error) => void;
-}
-export declare const ValidatingInput: <T>({ children, render, component, name, parse, format, onError, value, setValue, }: IValidatingInputProps<T>) => React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)> | null;
+};
+export declare const ValidatingInput: <T>(props: ValidatingInputProps<T> & RenderProps<ValidatingInputParams>) => JSX.Element | null;
 export default ValidatingInput;
