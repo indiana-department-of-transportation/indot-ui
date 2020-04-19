d4777b9f4b2283698b9fbe94040dd3b53a50dd02
jasmith79 <jasmith79@gmail.com>
2020-04-19 07:43:26 -0400

    Rebuild artifacts

--- packages/input/dist/input.d.ts
+++ packages/input/dist/input.d.ts
@@ -1,19 +1,25 @@
-/// <reference types="react" />
-import { FormControlEvent } from '@jasmith79/ts-utils';
+import { SyntheticEvent } from 'react';
 import { RenderProps } from '@jasmith79/react-utils';
 export declare type ValidatingInputProps<T> = {
     value: T;
-    setValue: (value: T | null) => void;
-    parse?: (input: string | null) => T | null;
-    format?: (value: T | null) => string;
+    setValue: (value: T) => void;
+    parse?: (input: string) => T;
+    format?: (value: T) => string;
     onError?: (err: Error) => void;
     name?: string;
 };
 export declare type ValidatingInputParams = {
     value: string;
     isError?: boolean;
-    onBlur: (evt: FormControlEvent) => void;
-    onChange: (evt: FormControlEvent) => void;
+    onBlur: (evt: SyntheticEvent) => void;
+    onChange: (evt: SyntheticEvent) => void;
+    name: string;
+};
+export declare const useValidatingInput: <T>({ value, setValue, parse, format, onError, name, }: ValidatingInputProps<T>) => {
+    value: string;
+    isError: boolean;
+    onBlur: (evt: SyntheticEvent<Element, Event>) => void;
+    onChange: (evt: SyntheticEvent<Element, Event>) => void;
     name: string;
 };
 export declare const ValidatingInput: <T>(props: ValidatingInputProps<T> & RenderProps<ValidatingInputParams>) => JSX.Element | null;
