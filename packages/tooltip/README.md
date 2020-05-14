483e174e84b953b38bd8f835b6adfc9ff1bb3985
jsmith <jasmith79@gmail.com>
2020-05-14 11:31:22 -0400

    Add Tooltip component

---
+++ packages/tooltip/README.md
@@ -0,0 +1,3 @@
+# NColumnGrid
+
+React component that arranges items in a 1, 2, or 3 column responsive grid.
---
+++ packages/tooltip/src/index.tsx
@@ -0,0 +1,78 @@
+/**
+ * index.tsx
+ *
+ * @description Wraps Material-UI tooltip components.
+ *
+ * @author jarsmith@indot.in.gov
+ * @license MIT
+ * @copyright INDOT, 2019
+ */
+
+import React, {
+  HTMLAttributes,
+  ReactNode,
+} from 'react';
+import clsx from 'clsx';
+
+import MUITooltip, { TooltipProps } from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+interface TooltipTitleProps<T> extends HTMLAttributes<T> {
+  content: ReactNode,
+}
+
+const useTooltipStyles = makeStyles({
+  title: {
+    fontSize: '1.5em',
+  },
+});
+
+/**
+ * @description Title element for tooltips.
+ *
+ * @param [props] The destructured React props.
+ * @param props.content The contents of the tooltip.
+ * @returns The TooltipTitle component. 
+ */
+export const TooltipTitle = (props: TooltipTitleProps<HTMLDivElement>) => {
+  const classes = useTooltipStyles();
+  const descendantProps = { ...props };
+  descendantProps.className = clsx(
+    classes.title,
+    descendantProps.className || '',
+  );
+
+  return <div {...descendantProps}>{props.content}</div>;
+};
+
+/**
+ * @description The TMC INDOT UI Tooltip component.
+ *
+ * @param [props] The destructured React props.
+ * @param props.title The title for the tooltip. If simply a string, wrapped in a TooltipTitle.
+ * @param props.children The React children to be displayed as the tooltip contents.
+ * @returns The Tooltip component.
+ */
+export const Tooltip = ({
+  title,
+  children,
+  ...rest
+}: TooltipProps) => {
+  const titleElem = typeof title === 'string'
+    ? <TooltipTitle content={title} />
+    : title;
+
+  const contentElem = typeof children === 'string'
+    ? <Button>{children}</Button>
+    : children;
+
+  return (
+    <MUITooltip {...rest} title={titleElem}>
+      {contentElem}
+    </MUITooltip>
+  );
+};
+
+export default Tooltip;
