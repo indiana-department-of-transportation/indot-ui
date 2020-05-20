9b0cf41472b585119668b2706ac7c80cb702d44c
jsmith <jasmith79@gmail.com>
2020-05-20 08:15:04 -0400

    Add logo package

---
+++ packages/logos/src/index.tsx
@@ -0,0 +1,23 @@
+/**
+ * index.tsx
+ *
+ * @description Main file for INDOT React Logo components.
+ * @author jarsmith@indot.in.gov
+ * @license MIT
+ * @copyright INDOT, 2020
+ */
+import React from 'react';
+import logo from './indot-logo';
+
+/**
+ * @description Logo with the Seal of Indiana.
+ */
+export const INDOTLogo = () => (
+  <img
+    src={`data:image/png;base64, ${logo}`}
+    alt="INDOT logo"
+    style={{ width: '48px', height: '48px' }}
+  />
+);
+
+export default INDOTLogo;
