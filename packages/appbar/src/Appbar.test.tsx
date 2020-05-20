f2e865401e2e28dcb3e3ed444ae65b7ef8fbfeca
jsmith <jasmith79@gmail.com>
2020-05-20 07:19:46 -0400

    Change Appbar implementation to something saner

--- packages/appbar/src/Appbar.test.tsx
+++ packages/appbar/src/Appbar.test.tsx
@@ -14,7 +14,7 @@ import Enzyme, { mount } from 'enzyme';
 
 import { MemoryRouter } from 'react-router-dom';
 
-import Appbar, { TMCUserAppbar } from './Appbar';
+import Appbar from './Appbar';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -29,75 +29,14 @@ describe('basic Appbar', () => {
     expect(wrapper.find('h6').text().trim()).toBe('foobar');
   });
 
-  it('should take a prop that renders a searchbar', () => {
-    const wrapper = mount(<MemoryRouter><Appbar title="foobar" /></MemoryRouter>);
-    expect(wrapper.find('input').length).toBe(0);
-    const wrapper2 = mount(
-      <MemoryRouter>
-        <Appbar title="foobar" Searchbar={<input />} />
-      </MemoryRouter>,
-    );
-    expect(wrapper2.find('input').length).toBe(1);
-  });
-});
-
-describe('user Appbar', () => {
-  it('should render without crashing', () => {
-    mount(<MemoryRouter><TMCUserAppbar title="" /></MemoryRouter>);
-    expect(true).toBe(true);
-  });
-
-  it('should have a required title prop that renders in an h6', () => {
-    const wrapper = mount(<MemoryRouter><TMCUserAppbar title="foobar" /></MemoryRouter>);
-    expect(wrapper.find('h6').text().trim()).toBe('foobar');
-  });
-
-  it('should take a prop that renders a searchbar', () => {
-    const wrapper = mount(<MemoryRouter><TMCUserAppbar title="foobar" /></MemoryRouter>);
-    expect(wrapper.find('input').length).toBe(0);
-    const wrapper2 = mount(
-      <MemoryRouter>
-        <TMCUserAppbar title="foobar" Searchbar={<input />} />
-      </MemoryRouter>,
-    );
-    expect(wrapper2.find('input').length).toBe(1);
-  });
-
-  it('should take a logoff prop.', () => {
-    const logoff = jest.fn();
-    const wrapper = mount(
-      <MemoryRouter>
-        <TMCUserAppbar
-          title="foobar"
-          logoff={logoff}
-        />
-      </MemoryRouter>
-    );
-
-    expect(wrapper.find(TMCUserAppbar).props().logoff).toBe(logoff);
-  });
-
-  it('should take a user prop.', () => {
-    const user = { userName: 'foobar', xyz: 123 };
-    const wrapper = mount(
-      <MemoryRouter>
-        <TMCUserAppbar
-          title="foobar"
-          user={user}
-        />
-      </MemoryRouter>
-    );
-
-    expect(wrapper.find(TMCUserAppbar).props().user).toBe(user);
-  });
-
-
-  it('should take an Account prop and render it', () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <TMCUserAppbar title="foobar" Account={<input />} />
-      </MemoryRouter>,
-    );
-    expect(wrapper.find('input').length).toBe(1);
-  });
+  // it('should take a prop that renders a searchbar', () => {
+  //   const wrapper = mount(<MemoryRouter><Appbar title="foobar" /></MemoryRouter>);
+  //   expect(wrapper.find('input').length).toBe(0);
+  //   const wrapper2 = mount(
+  //     <MemoryRouter>
+  //       <Appbar title="foobar" Searchbar={<input />} />
+  //     </MemoryRouter>,
+  //   );
+  //   expect(wrapper2.find('input').length).toBe(1);
+  // });
 });
--- packages/appbar/src/Appbar.tsx
+++ packages/appbar/src/Appbar.tsx
@@ -7,136 +7,83 @@
  * @copyright INDOT, 2019
  */
 
-import React, { ReactNode } from 'react';
+import React, {
+  ReactNode
+} from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
-import { emptyFn, IPojo } from '@jasmith79/ts-utils';
 
 import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import ToolBar, {
+  ToolbarProps
+} from '@material-ui/core/Toolbar';
 import { useTheme } from '@material-ui/core/styles';
-
-import Accnt from '@indot/account';
 
 import INDOTLogo from './indot-logo';
 
-interface IAppbarUser extends IPojo {
-  userName: string,
-}
+export const AppbarLogo = () => (
+  <Link to="/">
+    <img
+      src={`data:image/png;base64, ${INDOTLogo}`}
+      alt="INDOT logo"
+      style={{ width: '48px', height: '48px' }}
+    />
+  </Link>
+);
 
-interface IPlainAppBarProps {
-  title: string | ReactNode,
-  Searchbar?: ReactNode,
-}
+export type Titled = {
+  title: string,
+};
 
-interface ILoginAppbarProps extends IPlainAppBarProps {
-  Account: ReactNode,
-  user: IAppbarUser,
-  logoff: () => void,
-}
-
-/**
- * @description A simple app bar for TMC Applications.
- * @param {Object} [props] The destructured props object.
- * @param {string} props.title The header title.
- * @param {React.Component} props.Searchbar An optional searchbar component.
- * @returns {React.FunctionComponent} The Appbar component.
- */
-export const TMCAppbar = ({
-  title,
-  Searchbar,
-}: IPlainAppBarProps) => {
+export const AppbarTitle = ({
+  title
+}: Titled) => {
   const theme = useTheme();
   return (
-    <AppBar>
-      <ToolBar>
-        <Link to="/">
-          <img
-            src={`data:image/png;base64, ${INDOTLogo}`}
-            alt="INDOT logo"
-            style={{ width: '48px', height: '48px' }}
-          />
-        </Link>
-        <Typography variant="h6" style={{ marginLeft: theme.spacing(2) }}>
-          {title}
-        </Typography>
-        {Searchbar}
-      </ToolBar>
-    </AppBar>
-  )
+    <Typography variant="h6" style={{ marginLeft: theme.spacing(2) }}>
+      {title}
+    </Typography>
+  );
 };
 
-TMCAppbar.defaultProps = {
-  Searchbar: undefined,
-};
+export type AppbarBaseProps = {
+  className?: string,
+  ToolBarProps?: ToolbarProps,
+  children?: ReactNode,
+}
 
-TMCAppbar.propTypes = {
-  title: PropTypes.string.isRequired,
-  Searchbar: PropTypes.node,
-};
-
-/**
- * @description An app bar for TMC Applications with login and account management.
- * @param {Object} [props] The destructured props object.
- * @param {string} props.title The header title.
- * @param {React.Component} props.Searchbar An optional searchbar component.
- * @param {Object} props.user The logged in user.
- * @param {Function} props.logoff The logoff function.
- * @param {React.Component} props.Account An optional account management component.
- * @returns {React.FunctionComponent} The Appbar component.
- */
-export const TMCUserAppbar = ({
-  title,
-  Searchbar,
-  user = { userName: '' },
-  logoff = emptyFn,
-  Account = <Accnt userName={user.userName} logoff={logoff} />,
-}: ILoginAppbarProps) => {
-  const theme = useTheme();
+export const AppbarBase = ({
+  className = '',
+  ToolBarProps = {},
+  children,
+}: AppbarBaseProps) => {
   return (
-    <AppBar>
-      <ToolBar>
-        <Link to="/">
-          <img
-            src={INDOTLogo}
-            alt="INDOT logo"
-            style={{ width: '48px', height: '48px' }}
-          />
-        </Link>
-        <Typography variant="h6" style={{ marginLeft: theme.spacing(2) }}>
-          {title}
-        </Typography>
-        {Searchbar}
-        <div style={{
-          display: 'flex',
-          flexWrap: 'nowrap',
-          justifyContent: 'flex-end',
-          flexGrow: 4,
-        }}
-        >
-          {Account}
-        </div>
+    <AppBar className={className}>
+      <ToolBar {...ToolBarProps}>
+        {children}
       </ToolBar>
     </AppBar>
   );
 };
 
-TMCUserAppbar.defaultProps = {
-  user: { userName: '' },
-  logoff: emptyFn,
-  Account: <Accnt userName={''} logoff={emptyFn} />,
-};
+export type TMCAppbarProps = AppbarBaseProps & Titled;
 
-TMCUserAppbar.propTypes = {
-  title: PropTypes.string.isRequired,
-  Searchbar: PropTypes.node,
-  user: PropTypes.shape({
-    userName: PropTypes.string,
-  }),
-  logoff: PropTypes.func,
-  Account: PropTypes.node,
-};
+export const TMCAppbar = ({
+  title,
+  className,
+  ToolBarProps,
+  children
+}: TMCAppbarProps) => (
+  <AppbarBase
+    className={className}
+    ToolBarProps={ToolBarProps}
+  >
+    <AppbarLogo />
+    <AppbarTitle title={title} />
+    {children}
+  </AppbarBase>
+);
 
 export default TMCAppbar;
